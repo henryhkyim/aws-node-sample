79d5a773432dd3640a08a1057e21f127871d80df
Henry Yim <yimhingkwong@yahoo.com.hk>
2018-08-24 23:27:52 +0800

    initial upload

---
+++ src/app/components/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { render } from "react-dom"
+
+export class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h2>Hello world from App.js</h2>
+			</div>
+		)
+	}
+}
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+	mode: 'production',
+  entry: SRC_DIR + "/app/index.js",
+  output: {
+  	path: DIST_DIR + "/app",
+  	filename: "bundle.js",
+  	publicPath: "/app/"
+  },
+  module: {
+  	rules: [
+  		{
+  			test: /\.js?/,
+  			include: SRC_DIR,
+  			loaders: "babel-loader",
+  			query: {
+  				presets: ["react", "env", "stage-2"]
+  			}
+  		},
+      {
+        test: /\.css$/,
+        include: SRC_DIR,
+        loaders: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loaders: ['file-loader']
+      }
+  	]
+  },
+  devServer: {
+    historyApiFallback: true,
+  }
+};
+
+module.exports = config;
